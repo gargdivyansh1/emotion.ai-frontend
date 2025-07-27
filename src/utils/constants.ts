7ed6e43c3bc70048078d9c7f6fc23121cd1b95e2
gargdivyansh1 <divyanshgarg515@gmail.com>
2025-07-27 12:15:52 +0530

    public and private routes

---
+++ src/utils/constants.ts
@@ -0,0 +1,16 @@
+export const PUBLIC_ROUTES = [
+  "/",
+  "/authPage",
+  "/register",
+  "/auth/login",
+  "/docs",
+  "/second-docs",
+  "/about",
+  "/privacy",
+  "/support",
+  "/pricing",
+  "/blog",
+  "/blog/:id",
+  "/terms",
+  "/forum"
+];
